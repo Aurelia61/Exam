6129192096ae418f9dc70585e1193535750cf3ee
Aurelia61 <67269542+Aurelia61@users.noreply.github.com>
2020-11-25 22:42:57 +0100

    converter number OK

---
+++ Number/main.py
@@ -0,0 +1,17 @@
+# coding:utf-8
+
+from converter import Converter
+
+
+def main():
+    """
+        launch the program
+    """
+
+    Converter.ask_which_converter() 
+
+        
+
+# Program entry point
+if __name__ == "__main__":
+    main()
